8d2e0923d5650131045ceeaa4f0dd856eec2b5d0
Timofey Andreev <andreev.ta1@dns-shop.ru>
2023-05-25 18:36:48 +1000

    Attestation page

---
+++ array.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Array</title>
+</head>
+<body>
+<h1>Работа с массивами</h1>
+<h2>array_push</h2>
+<?php
+$array = [];
+for ($i = 0; $i < 10; $i +=1 ) {
+	array_push($array, $i);
+}
+
+foreach ($array as $element) {
+	echo "$element<br>";
+}
+?>
+<h2>array_pop</h2>
+<?php
+for ($i = 0; $i < 5; $i +=1 ) {
+	array_pop($array);
+}
+
+foreach ($array as $element) {
+	echo "$element<br>";
+}
+?>
+<h2>in_array</h2>
+<?php
+echo "2 в массиве:" . in_array(2, $array) . "<br>";
+echo "6 в массиве:" . in_array(6, $array) . "<br>";
+?>
+<h2>asort</h2>
+<?php
+$random_array = [];
+for ($i = 0; $i < 10; $i +=1 ) {
+	$elementOfArray = rand(1, 100);
+	array_push($random_array, $elementOfArray);
+}
+
+asort($random_array, 1);
+
+foreach ($random_array as $element) {
+	echo "$element<br>";
+}
+?>
+<h2>arsort</h2>
+<?php
+$random_array_2 = [];
+for ($i = 0; $i < 10; $i +=1 ) {
+	$elementOfArray = rand(1, 100);
+	array_push($random_array_2, $elementOfArray);
+}
+
+arsort($random_array_2, 1);
+
+foreach ($random_array_2 as $element) {
+	echo "$element<br>";
+}
+?>
+<h2>array_filter</h2>
+<?php
+$random_array_3 = [];
+for ($i = 0; $i < 10; $i +=1 ) {
+	$elementOfArray = rand(1, 100);
+	array_push($random_array_3, $elementOfArray);
+}
+
+function
+
+$random_array_3 = array_filter($random_array_3, () => {
+
+});
+
+foreach ($random_array_3 as $element) {
+	echo "$element<br>";
+}
+?>
+</body>
+</html>
